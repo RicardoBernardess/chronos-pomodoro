ea5bf8c49f3ed92e83c7b2cecd607e079aab12c2
Ricardo Bernardes <bernardess.ricardo@gmail.com>
2025-11-10 08:18:23 -0300

    Reducer

--- src/App.tsx
+++ src/App.tsx
@@ -11,5 +11,3 @@ export function App() {
     </TaskContextProvider>
   );
 }
-
-// TESTE
--- src/contexts/TaskContext/taskActions.ts
+++ src/contexts/TaskContext/taskActions.ts
@@ -10,14 +10,27 @@ import type { TaskModel } from '../../models/TaskModel';
 export enum TaskActionTypes {
   START_TASK = 'START_TASK',
   INTERRUPT_TASK = 'INTERRUPT_TASK',
+  RESET_STATE = 'RESET_STATE',
 }
 
-export type TaskActionModel =
+// export const TaskActionTypes = {
+//   START_TASK: 'START_TASK',
+//   INTERRUPT_TASK: 'INTERRUPT_TASK',
+// } as const;
+
+// export type TaskActionTypes = keyof typeof TaskActionTypes;
+
+export type TaskActionWithPayload = {
+  type: TaskActionTypes.START_TASK;
+  payload: TaskModel;
+};
+
+export type TaskActionsWithoutPayload =
   | {
-      type: TaskActionTypes.START_TASK;
-      payload: TaskModel;
+      type: TaskActionTypes.RESET_STATE;
     }
   | {
       type: TaskActionTypes.INTERRUPT_TASK;
-      payload: TaskModel;
     };
+
+export type TaskActionModel = TaskActionWithPayload | TaskActionsWithoutPayload;
