35d8f346b287f601f877173e270ed9afa9381106
Ricardo Bernardes <bernardess.ricardo@gmail.com>
2025-11-11 11:41:24 -0300

    TimerWorker

--- src/contexts/TaskContext/taskActions.ts
+++ src/contexts/TaskContext/taskActions.ts
@@ -7,30 +7,57 @@
 
 import type { TaskModel } from '../../models/TaskModel';
 
-export enum TaskActionTypes {
-  START_TASK = 'START_TASK',
-  INTERRUPT_TASK = 'INTERRUPT_TASK',
-  RESET_STATE = 'RESET_STATE',
-}
+// export enum TaskActionTypes {
+//   START_TASK = 'START_TASK',
+//   INTERRUPT_TASK = 'INTERRUPT_TASK',
+//   RESET_STATE = 'RESET_STATE',
+// }
 
-// export const TaskActionTypes = {
-//   START_TASK: 'START_TASK',
-//   INTERRUPT_TASK: 'INTERRUPT_TASK',
-// } as const;
+// export type TaskActionWithPayload = {
+//   type: TaskActionTypes.START_TASK;
+//   payload: TaskModel;
+// };
 
-// export type TaskActionTypes = keyof typeof TaskActionTypes;
+// export type TaskActionsWithoutPayload =
+//   | {
+//       type: TaskActionTypes.RESET_STATE;
+//     }
+//   | {
+//       type: TaskActionTypes.INTERRUPT_TASK;
+//     };
 
-export type TaskActionWithPayload = {
-  type: TaskActionTypes.START_TASK;
-  payload: TaskModel;
-};
+// export type TaskActionModel = TaskActionWithPayload | TaskActionsWithoutPayload;
+
+export const TaskActionTypes = {
+  START_TASK: 'START_TASK',
+  INTERRUPT_TASK: 'INTERRUPT_TASK',
+  RESET_STATE: 'RESET_STATE',
+  COUNT_DOWN: 'COUNT_DOWN',
+  COMPLETE_TASK: 'COMPLETE_TASK',
+} as const;
+
+export type TaskActionTypes =
+  (typeof TaskActionTypes)[keyof typeof TaskActionTypes];
+
+export type TaskActionWithPayload =
+  | {
+      type: typeof TaskActionTypes.START_TASK;
+      payload: TaskModel;
+    }
+  | {
+      type: typeof TaskActionTypes.COUNT_DOWN;
+      payload: { secondsRemaining: number };
+    };
 
 export type TaskActionsWithoutPayload =
   | {
-      type: TaskActionTypes.RESET_STATE;
+      type: typeof TaskActionTypes.RESET_STATE;
     }
   | {
-      type: TaskActionTypes.INTERRUPT_TASK;
+      type: typeof TaskActionTypes.INTERRUPT_TASK;
+    }
+  | {
+      type: typeof TaskActionTypes.COMPLETE_TASK;
     };
 
 export type TaskActionModel = TaskActionWithPayload | TaskActionsWithoutPayload;
